51c3b3fb5a4bafd190d623658781c03824df7b25
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 01:59:18 -0500

    Renamed run.py to app.py

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from application.cleaner import clean
+from application.controller.quiz_controller import generate_quiz 
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome to the Quiz Generator!"
+
+@app.route("/content", methods=["POST"])
+def receive_content():
+    notes_content = clean(request.get_data().decode("utf-8"))
+    print(f"Received data: {notes_content}")
+    return generate_quiz(notes_content).toJSON(), 200
