4cdd50937bf100dfcc6427907cfe04ce601da079
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 18:42:29 -0500

    Cards are fading in and out

--- frontend/src/lib/Card.svelte
+++ frontend/src/lib/Card.svelte
@@ -1,6 +1,7 @@
 <script>
   export let query;
   export let answer;
+  import { flip } from 'svelte/animate';
 
   let showAnswer = false;
 
@@ -9,18 +10,36 @@
   };
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="question-div" on:click={toggleAnswer}>
-  {#if showAnswer}
-    <p id="answer">{answer}</p>
-  {:else}
-    <p id="query">{query}</p>
-  {/if}
+  <div class={showAnswer ? 'shown' : 'hidden'}><p id="answer">{answer}</p></div>
+  <div class={showAnswer ? 'hidden' : 'shown'}><p id="query">{query}</p></div>
 </div>
 
 <style>
   .question-div {
-    margin: 10%;
     background-color: white;
+    height: 12vh;
+    width: 15vw;
+    margin: 10%;
     border-radius: 5px;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .shown {
+    opacity: 1;
+    transition: opacity 0.2s linear;
+  }
+
+  .hidden {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0s 0.2s, opacity 0.2s linear;
+  }
+
+  .question-div div {
+    grid-row-start: 1;
+    grid-column-start: 1;
   }
 </style>
