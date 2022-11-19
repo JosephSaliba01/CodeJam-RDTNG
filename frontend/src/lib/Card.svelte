d9d3d13cccb7e5679ec7d04a6d799a887b4d09ac
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 17:45:12 -0500

    Modified css

--- frontend/src/lib/Card.svelte
+++ frontend/src/lib/Card.svelte
@@ -3,12 +3,24 @@
   export let answer;
 
   let showAnswer = false;
+
+  let toggleAnswer = () => {
+    showAnswer = !showAnswer;
+  };
 </script>
 
-<div class="question-div">
+<div class="question-div" on:click={toggleAnswer}>
   {#if showAnswer}
     <p id="answer">{answer}</p>
   {:else}
     <p id="query">{query}</p>
   {/if}
 </div>
+
+<style>
+  .question-div {
+    margin: 10%;
+    background-color: white;
+    border-radius: 5px;
+  }
+</style>
