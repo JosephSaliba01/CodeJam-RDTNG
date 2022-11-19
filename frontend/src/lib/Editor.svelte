9398c3483fdc06f37e672883e835cb59f50d33ae
Joseph <josephsaliba54@gmail.com>
2022-11-19 07:20:28 -0500

    changes to front end

--- frontend/src/lib/Editor.svelte
+++ frontend/src/lib/Editor.svelte
@@ -30,9 +30,19 @@
     }
   })
 
-  let generateQuestions = () => {
-
+  let generateQuestions = async () => {
+    const data = editor.content;
+    console.log(data);
+    const response = await fetch('http://127.0.0.1:5000/content', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
   }
+  
 </script>
 
 {#if editor}
