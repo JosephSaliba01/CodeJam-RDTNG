d86212d20e5ddc5f8bcc63df45220b4a09d213c3
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 01:46:14 -0500

    Added svelte frontend

---
+++ frontend/src/lib/Editor.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { onMount, onDestroy } from 'svelte'
+  import { Editor } from '@tiptap/core'
+  import StarterKit from '@tiptap/starter-kit'
+  import CharacterCount from '@tiptap/extension-character-count'
+
+  let element
+  let editor
+
+  onMount(() => {
+    editor = new Editor({
+      element: element,
+      extensions: [
+        StarterKit,
+        CharacterCount
+      ],
+      content: '',
+      autofocus: true,
+      editable: true,
+      onTransaction: () => {
+        // force re-render so `editor.isActive` works as expected
+        editor = editor
+      },
+    })
+  })
+
+  onDestroy(() => {
+    if (editor) {
+      editor.destroy()
+    }
+  })
+
+  let generateQuestions = () => {
+
+  }
+</script>
+
+{#if editor}
+<!--  <button-->
+<!--          on:click={() => editor.chain().focus().toggleHeading({ level: 1}).run()}-->
+<!--          class:active={editor.isActive('heading', { level: 1 })}-->
+<!--  >-->
+<!--    H1-->
+<!--  </button>-->
+<!--  <button-->
+<!--          on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}-->
+<!--          class:active={editor.isActive('heading', { level: 2 })}-->
+<!--  >-->
+<!--    H2-->
+<!--  </button>-->
+<!--  <button on:click={() => editor.chain().focus().setParagraph().run()} class:active={editor.isActive('paragraph')}>-->
+<!--    P-->
+<!--  </button>-->
+{/if}
+
+<div bind:this={element}></div>
+
+{#if editor}
+  <p>{editor.storage.characterCount.words()} words</p>
+  <button on:click={generateQuestions}>Generate Questions</button>
+{/if}
