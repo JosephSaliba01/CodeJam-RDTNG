c7a5395ede9fb4fa5ee39660e494b0ff6535b1bf
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 15:36:25 -0500

    Fixed Cards

--- frontend/src/lib/Cards.svelte
+++ frontend/src/lib/Cards.svelte
@@ -1,10 +1,19 @@
 <script>
-    export let questions;
-    import Card from './Card.svelte';
+  import { onDestroy } from 'svelte';
+  import { currentNote } from '../store.js';
+  import Card from './Card.svelte';
+
+  let questions = null;
+
+  const unsubscribeToCurrentNote = currentNote.subscribe((value) => {
+    if (value != null) questions = value.questions;
+  });
+
+  onDestroy(() => unsubscribeToCurrentNote());
 </script>
 
-{#if Array.isArray($questions)}
-    {#each $questions as question}
-        <Card query={question.query} answer={question.answer} />
-    {/each}
+{#if Array.isArray(questions)}
+  {#each questions as question}
+    <Card query={question.query} answer={question.answer} />
+  {/each}
 {/if}
