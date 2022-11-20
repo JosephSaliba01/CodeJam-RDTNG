22fff809e3f3edda542e88cf4d3882df698fb84b
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-11-19 19:26:32 -0500

    did styling on storage

--- frontend/src/lib/Storage.svelte
+++ frontend/src/lib/Storage.svelte
@@ -23,9 +23,39 @@
 
 <div id="storage-div">
   <div id="storage-div-scrollable">
-    <button class="new_note_button" on:click={makeNewNote}>+ New Note</button>
-    {#each JSON.parse($allNotes) as note}
-      <button class="new_note_button" on:click={() => setCurrentNote(note)}>{note.title}</button>
-    {/each}
+    <button class="newNoteButton" on:click={makeNewNote}>+ New Note</button>
+    <div class="noteButtonContainer">
+      {#each JSON.parse($allNotes) as note}
+        <button class="noteButton" on:click={() => setCurrentNote(note)}
+          >{note.title}</button
+        >
+      {/each}
+    </div>
   </div>
 </div>
+
+<style>
+  .newNoteButton {
+    background-color: rgb(31, 144, 31);
+    color: white;
+    font-weight: 600;
+    font-size: 20px;
+  }
+
+  .newNoteButton:hover {
+    background-color: rgb(31, 117, 31);
+  }
+
+  .newNoteButton:active {
+    background-color: rgb(24, 93, 24);
+  }
+
+  .noteButtonContainer {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .noteButton {
+    width: 100%;
+  }
+</style>
